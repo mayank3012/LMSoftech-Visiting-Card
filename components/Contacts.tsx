69a4e89eedc02a7d604e68681ab00d6e439dcb03
Mayank Pandey <126338169+utamappdev@users.noreply.github.com>
2023-04-17 01:13:09 +0530

    first Commit

---
+++ components/Contacts.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const Contacts = () => {
+    return (
+        <div>
+            <ul className="">
+                <li className="">
+                    <div>
+                        <div >
+                            <button role="button" aria-label="email button" data-mdc-ripple-is-unbounded="true">
+                                <svg aria-hidden="true" focusable="false" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink">
+                                    <path d="M20 4H4C2.9 4 2.01 4.9 2.01 6L2 18C2 19.1 2.9 20 4 20H20C21.1 20 22 19.1 22 18V6C22 4.9 21.1 4 20 4ZM19.6 8.25L12.53 12.67C12.21 12.87
+                                     11.79 12.87 11.47 12.67L4.4 8.25C4.15 8.09 4 7.82 4 7.53C4 6.86 4.73 6.46 5.3 6.81L12 11L18.7 6.81C19.27 6.46 20 6.86 20 7.53C20 7.82 19.85 8.09 19.6 8.25Z"></path>
+                                </svg>
+                            </button>
+                        </div>
+                    </div>
+                    <span >
+                        <span >
+                            mayank.nice9411@gmail.com
+                        </span>
+                    </span>
+                </li>
+            </ul>
+        </div>
+    )
+}
+
+export default Contacts
---
+++ components/Info.tsx
@@ -0,0 +1,13 @@
+const Info = () => {
+    return (
+        <div className="vertical-line m-6 px-6 block ">
+            <div className="leading-9">
+                <h1 className="text-[1.75rem] font-extrabold">LM Softech</h1>
+                <h2 className="text-lg font-bold">Web Applications, Creative Itmes & Online Solutions</h2>
+                <h2 className="text-lg font-bold text-[var(--themeColor)]">Lucky Monga</h2>
+            </div>
+        </div>
+    )
+}
+
+export default Info
